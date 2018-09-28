0280cb7848aa3245bd542cd323676fbaf513df9b
Meher Vivek <vivekdheram@gmail.com>
2018-09-29 00:07:10 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='YAML-parser-and-emitter',
+    version='1.0',
+    description='A useful module for managing contacts as YAML',
+    author='Meher Vivek Dheram',
+    author_email='vivekdheram@gmail.com',
+    packages=['YAML-parser-and-emitter'],
+    install_requires=['pyYAML', 'argparse'],
+)
