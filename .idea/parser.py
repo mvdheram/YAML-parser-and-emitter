4b58e24546267fe6b944a8dc5ea5f81402f8f009
Meher Vivek <vivekdheram@gmail.com>
2018-09-28 20:58:26 +0200

    Changing the argparse parameter "required" and final adjustment of the code

--- .idea/parser.py
+++ .idea/parser.py
@@ -7,11 +7,12 @@ def dump(fname,data):
         dump = yaml.safe_dump(data, f,default_flow_style=False)
         f.close()
         return dump
+
 #load a file and return a python object of dictionary
 def load(fname):
     with open(fname) as f:
         data = yaml.safe_load(f)
-        #f.close()
+        f.close()
         return data
 
 #add new data to file
@@ -30,7 +31,7 @@ def show(fname,fdata):
     data = load(fname)
     for key,value in data.items():
         if fdata == key :
-            print (key,value)
+            return (key,value)
             break
         else:
             continue
@@ -38,29 +39,27 @@ def show(fname,fdata):
 
 
 def Main():
-    fname = "C:\Proj\YAML-parser-and-emitter\.idea\data1.yaml" #filepath to load from or dump to
-    dct1 = {"kimum": {"phone_number": 3, "city": "Karvina","Email":"kim@x-uni.com"}, "Davidum": {"phone_number": 33, "city": "Brno","Email":"d@uni.de"}}
-    parser = argparse.ArgumentParser(description='Yaml file loader and dumper for contacts')
-    parser.add_argument('-n','--name',metavar = '',required = True, help ='Name of the contact')
-    parser.add_argument('-ph','--phone_number',type = int,metavar = '',required = True, help = 'phone number of contact')
-    parser.add_argument('-c','--city',metavar = '',required = True, help ='city of contact')
-    parser.add_argument('-em','--email',metavar = '',required = True, help = 'email of the contact')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-q','--quite',action = 'store_true',help = 'print quite')
-    group.add_argument('-v','--verbose',action = 'store_true',help = 'print verbose')
-    args = parser.parse_args()
-    add(args.name,args.phone_number,args.city,args.email,fname)
-    if args.quite :
-        print (list(fname))
-    elif args.verbose:
-        print ("\nlist of contacts in file name: " + fname + " are:\n\n"  + list(fname))
-    else:
-        print ("\nlist :\n\n" + list(fname))
+    fname = "data.yaml"
+parser = argparse.ArgumentParser(description='Yaml file loader and dumper for contacts')
+parser.add_argument('-a','--add_name',metavar = '', help ='Name of the contact')
+parser.add_argument('-ph','--phone_number',type = int,metavar = '', help = 'phone number of contact')
+parser.add_argument('-c','--city',metavar = '', help ='city of contact')
+parser.add_argument('-em','--email',metavar = '', help = 'email of the contact')
+parser.add_argument('-sh','--show',metavar = '', help = 'show the contact')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-ls','--list',action = 'store_true',help = 'print  list')
+group.add_argument('-v','--verbose',action = 'store_true',help = 'print verbose list ')
+args = parser.parse_args()
+data = add(args.add_name,args.phone_number,args.city,args.email,fname)
+
+if args.show:
+    print(show(fname,args.show))
+
+if args.list:
+    print(list(fname))
+elif args.verbose:
+    print ("\nlist of contacts in file name: " + fname + " are:\n\n"  + list(fname))
 
 
 if __name__ == "__main__":
     Main()
-    # dump(fname, dct1)
-    # load(fname)
-    print (list(fname))
-    show(fname,"kimum")
